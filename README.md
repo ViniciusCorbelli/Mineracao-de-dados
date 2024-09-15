0ddd3f0501c3966ea801dec031b64d46532f957e
B4Hive <b4.bruno.santos@gmail.com>
2024-09-15 02:09:28 -0300

    incluido filtro de arquivos e extensoes ignoradas

--- README.md
+++ README.md
@@ -15,19 +15,9 @@ mvn clean install
 ```
 - Run the jar created in the target directory 
 ```
-java -jar target/GitMining-1.0-SNAPSHOT-jar-with-dependencies.jar kind_of_analysis repository_information [GitHub_Token]
+java -jar target/GitMining-1.0-SNAPSHOT-jar-with-dependencies.jar /path/local/repo
 ```
-The kind_of_analysis can be **remote** or **local**. 
-- For **remote** analysis, the user should inform the URL to 
-a valid project on GitHub. In the case of private projects, the user needs to give a GitHub_Token. For instance:
+The user should inform a local path to a Git repository. For example:
 ```
-java -jar target/GitMining-1.0-SNAPSHOT-jar-with-dependencies.jar remote https://github.com/voldemort/voldemort
+java -jar target/GitMining-1.0-SNAPSHOT-jar-with-dependencies.jar C:\Users\Public\Downloads\repo\Mineracao-de-dados
 ```
-- For **local** analysis, the user should inform a local path to a Git repository. For example:
-```
-java -jar target/GitMining-1.0-SNAPSHOT-jar-with-dependencies.jar "/media/gleiph/Gleiph dados/repositories/voldemort"
-```
-
-If you have any doubts or suggestions, please let me know by sending an email to gleiph@ice.ufjf.br. 
-
- 
--- src/main/java/br/ufjf/dcc/dcc093/gitmining/GitMining.java
+++ src/main/java/br/ufjf/dcc/dcc093/gitmining/GitMining.java
@@ -2,8 +2,8 @@ package br.ufjf.dcc.dcc093.gitmining;
 
 import java.io.FileWriter;
 import java.io.IOException;
-import java.text.DecimalFormat;
 import java.util.List;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -14,69 +14,132 @@ import br.ufjf.dcc.dcc093.gitmining.model.Commit;
 import br.ufjf.dcc.dcc093.gitmining.model.File;
 
 public class GitMining {
+
+    private static final Set<String> IGNORED_EXTENSIONS = Set.of(
+            "md", "yml", "yaml", "json", "xml", "properties", "csv",
+            "log", "tmp", "bak", "swp", "class", "jar", "war", "ear",
+            "sqlite", "db", "pem", "crt", "p12", "key", "classpath",
+            "jsp", "prefs", "txt", "sql", "tag", "jmx", "html", "js",
+            "springBeans", "iml", "cmd", "springjavaformatconfig", "lock",
+            "editorconfig", "markdown", "dtd"
+    );
+
+    private static final Set<String> IGNORED_FILES = Set.of(
+            "README", ".gitignore", "pom.xml", "build.gradle", "package.json",
+            "LICENSE", "gradlew", "Makefile", ".springBeans"
+    );
+
+    private static String getFileExtension(String filePath) {
+        int lastDotIndex = filePath.lastIndexOf('.');
+        return (lastDotIndex == -1) ? "" : filePath.substring(lastDotIndex + 1).toLowerCase();
+    }
+
+    private static String getFileName(String filePath) {
+        int lastSlashIndex = filePath.lastIndexOf('/');
+        return (lastSlashIndex == -1) ? filePath : filePath.substring(lastSlashIndex + 1);
+    }
+
+    private static boolean isIgnoredFile(String filePath) {
+        String extension = getFileExtension(filePath);
+        String fileName = getFileName(filePath);
+
+        return IGNORED_EXTENSIONS.contains(extension) || IGNORED_FILES.contains(fileName);
+    }
+
     public static void main(String[] args) throws IOException {
         if (args.length != 1) {
             System.out.println("Please inform the repository path.");
             return;
         }
-
+        System.out.println("Programa em execucao, por favor aguarde.");
         CLIExecute executer = new CLIExecute(args[0]);
         GitCLI gitCLI = new GitCLI(args[0]);
 
         List<Commit> commits = gitCLI.log();
 
         try (FileWriter csvWriter = new FileWriter("commits.csv")) {
-            csvWriter.append("Commit Hash|Autor|Nome|Inserções|Deleções|Diferença de Porcentagem\n");
+            csvWriter.append("Commit Hash,Autor,Nome,Inserções,Deleções,Diferença de Porcentagem\n");
 
+            char[] loading = {'\\','-','|','/','|'};
+            int loadIndex = 0;
             for (Commit commit : commits) {
+                System.out.print(loading[loadIndex]);
+                loadIndex++;
+                if (loadIndex >= loading.length) {
+                    loadIndex = 0;
+                }
+
                 List<File> files = gitCLI.show(commit.getHash());
 
-                for (File file : files) {
-                    CLIExecution cat = executer.execute("git show --stat " + commit.getHash() + " " + file.getPath());
-                    List<String> output = cat.getOutput();
+                analysingFiles(commit, files, executer, csvWriter);
 
-                    if (!output.isEmpty()) {
-                        String lastLine = output.get(output.size() - 1);
+                System.out.print("\r");
+                System.out.print("                    ");
+                System.out.print("\r");
+                System.out.flush();
+            }
 
-                        Pattern patternInsertions = Pattern.compile("(\\d+) insertions?\\(\\+\\)");
-                        Pattern patternDeletions = Pattern.compile("(\\d+) deletions?\\(-\\)");
+            System.out.println("Arquivo criado com sucesso: commits.csv");
+        } catch (IOException e) {
+            System.err.println("Erro ao criar o arquivo: " + e.getMessage());
+        }
+    }
 
-                        Matcher matcherInsertions = patternInsertions.matcher(lastLine);
-                        Matcher matcherDeletions = patternDeletions.matcher(lastLine);
+    private static void analysingFiles(Commit commit, List<File> files, CLIExecute executer, FileWriter csvWriter) throws IOException {
+        for (File file : files) {
+            if (isIgnoredFile(file.getPath())) {
+                continue;
+            }
 
-                        int insertions = 0;
-                        int deletions = 0;
+            CLIExecution cat = executer.execute("git show --stat " + commit.getHash() + " " + file.getPath());
+            List<String> output = cat.getOutput();
 
-                        if (matcherInsertions.find()) {
-                            insertions = Integer.parseInt(matcherInsertions.group(1));
-                        }
+            if (!output.isEmpty()) {
+                String lastLine = output.get(output.size() - 1);
 
-                        if (matcherDeletions.find()) {
-                            deletions = Integer.parseInt(matcherDeletions.group(1));
-                        }
+                Pattern patternInsertions = Pattern.compile("(\\d+) insertions?\\(\\+\\)");
+                Pattern patternDeletions = Pattern.compile("(\\d+) deletions?\\(-\\)");
 
-                        int totalChanges = insertions + deletions;
-                        if (totalChanges > 0) {
-                            double percentageDifference = Math.abs(insertions - deletions) / (double) totalChanges * 100;
+                Matcher matcherInsertions = patternInsertions.matcher(lastLine);
+                Matcher matcherDeletions = patternDeletions.matcher(lastLine);
 
-                            if (percentageDifference <= 10) {
-                                DecimalFormat df = new DecimalFormat("#.00");
-                                csvWriter.append(commit.getHash()).append('|')
-                                        .append(commit.getCommitterEmail()).append('|')
-                                        .append(commit.getCommitterName()).append('|')
-                                        .append(String.valueOf(insertions)).append('|')
-                                        .append(String.valueOf(deletions)).append('|')
-                                        .append(df.format(percentageDifference)).append('\n');
-                                break;
-                            }
-                        }
+                int insertions = 0;
+                int deletions = 0;
+
+                if (matcherInsertions.find()) {
+                    insertions = Integer.parseInt(matcherInsertions.group(1));
+                }
+
+                if (matcherDeletions.find()) {
+                    deletions = Integer.parseInt(matcherDeletions.group(1));
+                }
+
+                int totalChanges = insertions + deletions;
+                if (totalChanges > 0) {
+                    double percentageDifference = Math.abs(insertions - deletions) / (double) totalChanges * 100;
+
+                    if (percentageDifference <= 10) {
+                        String line = (commit.getHash() != null ? commit.getHash() : "") +
+                                '|' +
+                                (commit.getCommitterEmail() != null ? commit.getCommitterEmail() : "") +
+                                '|' +
+                                (commit.getCommitterName() != null ? commit.getCommitterName() : "") +
+                                '|' +
+                                insertions +
+                                '|' +
+                                deletions +
+                                '|' +
+                                String.format("%.2f", percentageDifference) +
+                                '\n';
+
+                        line = line.replace(',','.');
+                        line = line.replace('|',',');
+                        csvWriter.append(line);
+                        break;
                     }
                 }
             }
-
-            System.out.println("Arquivo CSV criado com sucesso: commits.csv");
-        } catch (IOException e) {
-            System.err.println("Erro ao criar o arquivo CSV: " + e.getMessage());
         }
     }
+
 }
